bafe3cd868503e457ea9be11621f4a32090ff1c1
d99b09 <41631211+d99b09@users.noreply.github.com>
2018-10-17 21:31:33 +0300

    Delete genetic_error.py

--- genetic_error.py
+++
@@ -1,8 +0,0 @@
-class AIFHError(Exception):
-    """An error was raised. This is used for several purposes, see individual error messages."""
-
-    def __init__(self, value):
-        self.value = value
-
-    def __str__(self):
-        return repr(self.value)
