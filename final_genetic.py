4b96154a40275f34c51967c581466160cf1f44d2
d99b09 <41631211+d99b09@users.noreply.github.com>
2018-10-29 17:13:47 +0300

    Update final_genetic.py

--- final_genetic.py
+++ final_genetic.py
@@ -1,5 +1,8 @@
 import math
 import random
+import time
+
+time.process_time()
 
 def random_q(number_q):
     out_vector=[]
@@ -13,31 +16,43 @@ def random_X(number_q, num_of_select):
         out_vector.append(random_q(number_q))
     return out_vector
 
+def find_xyz(num_q, q, par_l,  par_q):
+    x=0
+    y=0
+    z=0
+    q_x=0
+    q_y1=0
+    q_y0=0
+    q_z=0
 
-def score_funct(member,T):
-    # Parametrs
-    l = 5
-    #then add to function
-    q = member
-    result = math.sqrt(l * (
-                (math.cos(q[0] + q[1] + q[2]) - T[0][0]) ** 2 + (-math.sin(q[0] + q[1] + q[2]) - T[0][1]) ** 2 + (
-                    math.cos(q[0] + q[1] + q[2]) - T[1][1]) ** 2 + (
-                            math.sin(q[0] + q[1] + q[2]) - T[1][0]) ** 2) + (
-                                    l * (math.cos(q[0] + q[1] + q[2]) + math.cos(q[0] + q[1]) + math.cos(q[0])) -
-                                    T[0][3]) ** 2 + (
-                                    l * (math.sin(q[0] + q[1] + q[2]) + math.sin(q[0] + q[1]) + math.sin(q[0])) -
-                                    T[1][3]) ** 2)
+    for i in range(num_q):
+        if par_q[i]:
+            q_x += q[i]
+            q_y1 += q[i]
+            x += par_l[i] * math.cos(q_y1)
+            y += par_l[i] * math.sin(q_z)
+        else:
+            q_y0 += q[i]
+            q_z += q[i]
+            y += par_l[i] * math.cos(q_y0)
+            z += par_l[i] * math.sin(q_z)
+    return x, y, z
+
+def score_funct(member, T, par_l, par_q):
+    num_q=len(member)
+    x, y, z = find_xyz(num_q, member, par_l, par_q)
+    result = math.sqrt((x - T[0]) ** 2 + (y - T[1]) ** 2 + (z - T[2]) ** 2)
     return result
 
 def mutate(member):
-    T=member
+    T = member
     mutated = []
 
     for i in range(len(T)):
         input_vector=list(T[i])
         max_index=len(input_vector)
         mutated_index = random.choice(range(0, max_index))
-        mutation_scalar=4*random.random()*input_vector[mutated_index]
+        mutation_scalar=2*random.random()*input_vector[mutated_index]
         output_vector = input_vector[:]
         output_vector[mutated_index] = mutation_scalar
         mutated.append(output_vector)
@@ -46,40 +61,41 @@ def mutate(member):
 def reproduce(member, k):
     output = []
     for i in range(0, k):
-        mut=mutate(member)
+        mut = mutate(member)
         for j in range(len(mut)):
-            vec=mut[j]
+            vec = mut[j]
             output.append(vec)
 
     return output
 
-def select(offsprings, size,T):
-    survival_value = map(lambda x: (score_funct(x,T), x), offsprings)
+def select(offsprings, size, T, par_l, par_q):
+    survival_value = map(lambda x: (score_funct(x, T, par_l, par_q), x), offsprings)
     select = list(map(lambda xy: xy[1], sorted(survival_value)[:size]))
     return select
 
-def next_generation(generation, offspring_size,T):
-    survival_size=len(generation)
+def next_generation(generation, offspring_size,T, par_l, par_q):
+    survival_size = len(generation)
     offsprings = []
     offsprings.append(generation[0])
     offsprings += reproduce(generation, offspring_size)
-    next_generation = select(offsprings, survival_size,T)
+    next_generation = select(offsprings, survival_size, T, par_l, par_q)
     return next_generation
 
-#T-матрица положения 4х4
-#num_of_select-число "выживших потомков" образующихся после каждого нового отбора 
-#max_num_generations-число генераций
-#num_of_offsprings-число "потомков у каждого вектора
-def evolution(number_q,T,num_of_select,max_num_generations=2000):
-    generation=random_X(number_q, num_of_select)
-    num_of_offsprings = 25
+def evolution(number_q, T, par_l, par_q, num_of_select=20, num_of_offsprings = 40, max_num_generations=1000):
+    generation = random_X(number_q, num_of_select)
     generation_index = 1
     while True:
-        generation = next_generation(generation, num_of_offsprings,T)
+        generation = next_generation(generation, num_of_offsprings, T, par_l, par_q)
         generation_index += 1
         if generation_index > max_num_generations:
             break
     q=generation[0]
-    result=score_funct(q,T)
-
+    result=score_funct(q, T, par_l, par_q)
     return q, result
+
+T = [0, 10, 8]
+par_l=[5, 5, 5]
+par_q=[0, 0, 0]
+
+print(evolution(3, T, par_l, par_q))
+print(time.process_time())
